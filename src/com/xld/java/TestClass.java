e317a4e0806fc41af90ade80e61d1f51ccf5cf2e
ConcaSu <453813781@qq.com>
2017-07-27 16:02:14 +0800

    Add print I wanna try for another time

--- src/com/xld/java/TestClass.java
+++ src/com/xld/java/TestClass.java
@@ -5,5 +5,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		System.out.println("I wanna try for another time!");
 	}
 }
